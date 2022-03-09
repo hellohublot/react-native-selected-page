576f0137a01281f43ba44c18864c9076b2ff5ea1
hublot <hublot@aliyun.com>
2022-03-09 21:55:13 +0800

    replace android viewpager to scrollview

--- src/HTPageContentView.js
+++ src/HTPageContentView.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList, Platform, Animated, Dimensions } from 'react-native'
-import ViewPager from 'react-native-pager-view'
+import { View, Text, FlatList, Animated, Dimensions } from 'react-native'
 import PropTypes from 'prop-types'
 
 const SCREEN_WIDTH = Dimensions.get('window').width
 
 const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
-
-const AnimatedViewPager = Animated.createAnimatedComponent(ViewPager)
 
 export default class HTPagContentView extends Component {
 
@@ -31,27 +28,14 @@ export default class HTPagContentView extends Component {
 	constructor(props) {
 		super(props)
 		let initScrollIndex = this.props.initScrollIndex ?? 0
-		let animatedNativeEvent = {}
-		if (Platform.OS == 'ios') {
-			this._contentOffsetValueX = new Animated.Value(initScrollIndex * this.scrollViewWidth())
-			this._contentSizeValueWidth = new Animated.Value(this.scrollViewWidth())
-			this.scrollPageIndexValue = Animated.divide(this._contentOffsetValueX, this._contentSizeValueWidth)
-			animatedNativeEvent = {
-				contentOffset: {
-                    x: this._contentOffsetValueX
-                },
-			}
-		} else {
-			this._offsetValue = new Animated.Value(0)
-			this._positionValue = new Animated.Value(initScrollIndex)
-			this.scrollPageIndexValue = Animated.add(this._offsetValue, this._positionValue)
-			animatedNativeEvent = {
-				offset: this._offsetValue,
-                position: this._positionValue
-			}
-		}
+		this._contentOffsetValueX = new Animated.Value(initScrollIndex * this.scrollViewWidth())
+		this._layoutValueWidth = new Animated.Value(this.scrollViewWidth())
+		this.scrollPageIndexValue = Animated.divide(this._contentOffsetValueX, this._layoutValueWidth)
 		this.event = Animated.event([{
-            nativeEvent: animatedNativeEvent
+            nativeEvent: {
+            	contentOffset: { x: this._contentOffsetValueX },
+            	layoutMeasurement: { width: this._layoutValueWidth }
+            }
         }], {
             useNativeDriver: true
         })
@@ -67,25 +51,19 @@ export default class HTPagContentView extends Component {
 
 	scrollPageIndex = (pageIndex, animated = true) => {
 		try {
-			if (Platform.OS == 'ios') {
-				let scrollConfig = { index: pageIndex, animated: animated }
-				this?.scrollView?.scrollToIndex && this?.scrollView?.scrollToIndex(scrollConfig)
-			} else {
-				if (animated) {
-					this?.scrollView?.setPage && this.scrollView.setPage(pageIndex)
-				} else {
-					this?.scrollView?.setPageWithoutAnimation && this.scrollView.setPageWithoutAnimation(pageIndex)
-				}
-			}
+			let scrollConfig = { index: pageIndex, animated: animated }
+			this?.scrollView?.scrollToIndex && this?.scrollView?.scrollToIndex(scrollConfig)
 		} catch(e) {
 		}
 	}
 
 	_renderItem = (item, index, useViewPager = false) => {
+		let keyExtractor = this.props.keyExtractor ?? ((item, index) => index)
+		let key = keyExtractor(item, index)
 		return (
-			<View key={index} style={{ width: this.scrollViewWidth(), height: '100%' }}>
+			<View key={key} style={{ width: this.scrollViewWidth(), height: '100%' }}>
 			{
-				this.props.renderItem({item, index})
+				this.props.renderItem({ item, index })
 			}
 			</View>
 		)
@@ -98,40 +76,25 @@ export default class HTPagContentView extends Component {
 	}
 
 	render() {
-		return Platform.select({
-			ios: (
-				<AnimatedFlatList
-					{...this.props}
-					ref={this._ref}
-					style={{ width: '100%', ...this?.props?.style }}
-					renderItem={({item, index}) => this._renderItem(item, index)}
-					onScroll={this.event}
-					getItemLayout={(item, index) => ({
-						length: this.scrollViewWidth(),
-						offset: index * this.scrollViewWidth(),
-						index
-					})}
-					contentOffset={{ x: (this.props?.initScrollIndex ?? 0) * this.scrollViewWidth(), y: 0 }}
-				/>
-			),
-			android: (
-				<View style={{flex: 1, overflow: 'hidden'}}>
-					<AnimatedViewPager
-						{...this.props}
-						ref={this._ref}
-						style={{ width: '100%', height: '100%', ...this?.props?.style }}
-						initialPage={this.props.initScrollIndex}
-						onPageScroll={this.event}
-					>
-						{
-							this.props.data.map((item, index) => {
-								return this._renderItem(item, index, true)
-							})
-						}
-					</AnimatedViewPager>
-				</View>
-			)
-		})
+		return (
+			<AnimatedFlatList
+				{...this.props}
+				ref={ref => {
+					this._ref(ref)
+					this.props.scrollViewRef && this.props.scrollViewRef(ref)
+				}}
+
+				style={{ width: '100%', ...this?.props?.style }}
+				renderItem={({item, index}) => this._renderItem(item, index)}
+				onScroll={this.event}
+				getItemLayout={(item, index) => ({
+					length: this.scrollViewWidth(),
+					offset: index * this.scrollViewWidth(),
+					index
+				})}
+				contentOffset={{ x: (this.props?.initScrollIndex ?? 0) * this.scrollViewWidth(), y: 0 }}
+			/>
+		)
 	}
 
 }
